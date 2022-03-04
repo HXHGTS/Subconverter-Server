a15f45bae2b03deb31998b19182e1b6310a6edef
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-03-04 23:24:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ cd /root/subconverter && chmod +x subconverter
 
 echo 正在写入配置文件. . .
 
-curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/pref.ini > /root/subconverter/pref.ini
-
 curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/pref.toml > /root/subconverter/pref.toml
 
 curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/subconverter.service > /etc/systemd/system/subconverter.service
