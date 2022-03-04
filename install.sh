639045c5b20b8600323256a5481b191e34aebfa2
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-03-04 22:56:27 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo 正在下载并安装Subconverter. . .
+
+wget https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz -O subconverter_linux64.tar.gz
+
+tar -zxvf subconverter_linux64.tar.gz
+
+rm -f subconverter_linux64.tar.gz
+
+cd /root/subconverter && chmod +x subconverter
+
+echo 正在写入配置文件. . .
+
+curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/pref.ini > /root/subconverter/pref.ini
+
+curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/pref.toml > /root/subconverter/pref.toml
+
+curl https://raw.githubusercontent.com/HXHGTS/Subconverter-Server/main/subconverter.service > /etc/systemd/system/subconverter.service
+
+echo 正在启动Subconverter并写入开机引导. . .
+
+systemctl enable subconverter
+
+systemctl start subconverter
+
+exit 0
